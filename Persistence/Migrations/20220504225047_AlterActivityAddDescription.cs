c79ea8e2b2b61b19c6effc687184abdb3ccc7458
Kevin Firman <kevindfirman@outlook.com>
2022-05-10 22:23:15 -0400

    End of section 2

---
+++ Persistence/Migrations/20220504225047_AlterActivityAddDescription.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class AlterActivityAddDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Activities",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Activities");
+        }
+    }
+}
